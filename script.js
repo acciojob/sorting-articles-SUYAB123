bb77694b5a396bcbf3a59425b50681f559a450e5
SUYAB123 <111383970+SUYAB123@users.noreply.github.com>
2025-01-20 14:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//your JS code here. If required.
+const bands = [
+  'The Plot in You', 
+  'The Devil Wears Prada', 
+  'Pierce the Veil', 
+  'Norma Jean', 
+  'The Bled', 
+  'Say Anything', 
+  'The Midway State', 
+  'We Came as Romans', 
+  'Counterparts', 
+  'Oh, Sleeper', 
+  'A Skylit Drive', 
+  'Anywhere But Here', 
+  'An Old Dog'
+];
+
+function sortBands(bands) {
+  const ignoreWords = ['a', 'an', 'the'];
+
+  return bands.sort((a, b) => {
+    const cleanName = (name) => {
+      let words = name.toLowerCase().split(' '); 
+      while (ignoreWords.includes(words[0])) { 
+        words.shift();
+      }
+      return words.join(' ');  
+    };
+
+    // Compare the cleaned names
+    if (cleanName(a) < cleanName(b)) return -1;
+    if (cleanName(a) > cleanName(b)) return 1;
+    return 0;
+  });
+}
+const sortedBands = sortBands(bands);
+
+const bandList = document.getElementById('band');
+sortedBands.forEach(band => {
+  const li = document.createElement('li');  
+  li.textContent = band;  
+  bandList.appendChild(li); 
+});
+
